5b828fe116a44a7f18856a41655c55588394cf82
Reshma <reshma.dileep@qualityarc.com>
2019-01-31 11:28:03 +1100

    Added TestNG dependancy to pom.xml

--- src/test/java/Browser.java
+++ src/test/java/Browser.java
@@ -1,19 +1,33 @@
 
 import java.net.MalformedURLException;
 
+import org.testng.annotations.Test;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class Browser extends Base {
 
-	public static void main(String[] args) throws MalformedURLException {
-// TODO Auto-generated method stub
+//	public static void main(String[] args) throws MalformedURLException {
+//// TODO Auto-generated method stub
+//		AndroidDriver<AndroidElement> driver = capabilities();
+//		driver.get("http://facebook.com");
+//		driver.findElementByXPath("//*[@id='u_0_1']/div[1]/div/input").sendKeys("qwerty");
+//		driver.findElementByName("pass").sendKeys("12345");
+//		driver.findElementByXPath("//button[@value='Log In']").click();
+//
+//	}
+//	
+	
+	@Test
+	public void startTest() throws MalformedURLException {
 		AndroidDriver<AndroidElement> driver = capabilities();
 		driver.get("http://facebook.com");
 		driver.findElementByXPath("//*[@id='u_0_1']/div[1]/div/input").sendKeys("qwerty");
 		driver.findElementByName("pass").sendKeys("12345");
 		driver.findElementByXPath("//button[@value='Log In']").click();
-
+		
+		
 	}
 
 }
