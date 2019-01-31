aba50731c929aaac7881099ebfb5dc902f7fc41a
Tanvi Thapar <tanvi.thapar@qualityarc.com>
2019-01-31 18:16:23 +1100

    Cucumber report generation changes

--- src/test/java/runner/testRunner.java
+++ src/test/java/runner/testRunner.java
@@ -1,8 +1,17 @@
 package runner;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.AfterSuite;
+
 import cucumber.api.CucumberOptions;
 
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
 @CucumberOptions(features = "src/test/java/features/",
         glue = { "stepDefinitions" },
         tags= {"not (@inProgress or @manualTest or @todoDev)"},
@@ -12,5 +21,37 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
         dryRun = false,
         strict = false)
 public class testRunner extends AbstractTestNGCucumberTests {
+	 @AfterSuite
+	    public static void writeCucumberHtmlReport() {
+	        File reportOutputDirectory = new File("target");
+	        List<String> jsonFiles = new ArrayList<>();
+	        jsonFiles.add("./target/cucumber-reports/Cucumber.json");
+
+
+	        String buildNumber = "1";
+	        String projectName = "Appium Java";
+	        boolean runWithJenkins = false;
+//	    	boolean parallelTesting = false;
+
+	        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+	        // optional configuration - check javadoc
+	        configuration.setRunWithJenkins(runWithJenkins);
+	        configuration.setBuildNumber(buildNumber);
+	        // Additional metadata presented on main page
+	        configuration.addClassifications("Platform", "Windows");
+	    	configuration.addClassifications("Browser", "Chrome");
+
+	        // optionally add metadata presented on main page via properties file
+//	    	List<String> classificationFiles = new ArrayList<>();
+//	    	classificationFiles.add("properties-1.properties");
+//	    	classificationFiles.add("properties-2.properties");
+//	    	configuration.addClassificationFiles(classificationFiles);
+
+	        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+	        @SuppressWarnings("unused")
+	        Reportable result = reportBuilder.generateReports();
+	        // and here validate 'result' to decide what to do
+	        // if report has failed features, undefined steps etc
+	    }
 
 }
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -4,8 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmenter;
 
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -37,5 +42,15 @@ public class Hooks extends Base{
 	         this.world.context.put("testEnv", testEnv.toLowerCase());
 	         world.context.put("driver", driver);
 }
+	    @After
+	    public void doCleanupAfterExecution(Scenario scenario){
+	        if (scenario.isFailed()) {
+	            TakesScreenshot browser = (TakesScreenshot) new Augmenter().augment(driver);
+	            final byte[] screenshot = browser.getScreenshotAs(OutputType.BYTES);
+	            scenario.embed(screenshot, "image/png");
+	            scenario.write("URL: " + driver.getCurrentUrl());
+	        }
+	        driver.close();
+	    }
 }
 
