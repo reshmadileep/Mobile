7994333cf256ec9e08ff6b63c798404a0cf9613d
naveen1295 <42132518+naveen1295@users.noreply.github.com>
2019-02-01 11:04:21 +1100

    refactoring and modifications to run through pom

--- src/test/java/stepDefinitions/login.java
+++ src/test/java/stepDefinitions/login.java
@@ -1,22 +1,17 @@
 package stepDefinitions;
 
-import org.openqa.selenium.WebDriver;
-
-import QualityArc.Mobile.Browser;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import utils.Base;
-
 import pages.HomePage;
 
 public class login {
 	public World world;
 	public AndroidDriver<AndroidElement> driver;
 	public HomePage homePage;
-	
+
 	public login(World world) {
 		this.world = world;
 		driver = (AndroidDriver<AndroidElement>) this.world.context.get("driver");
@@ -25,29 +20,26 @@ public class login {
 
 	@Given("I as a admin user navigate to Home page")
 
-public void i_as_a_admin_user_navigate_to_Home_page() {
-    // Write code here that turns the phrase above into concrete actions
-			driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
-			
-		
-		
+	public void i_as_a_admin_user_navigate_to_Home_page() {
+		// Write code here that turns the phrase above into concrete actions
+		driver.get("https://orangehrm-demo-6x.orangehrmlive.com/");
+
+	}
+
+	@When("I try to login with username {string} and password {string}")
+	public void i_try_to_login_with_username_and_password(String string, String string2) {
+		// Write code here that turns the phrase above into concrete actions
+
+		homePage.enterUsername("Admin");
+		homePage.enterPassword("admin123");
+		homePage.clickLoginBtn();
+	}
+
+	@Then("I am logged in successfully")
+	public void i_am_logged_in_successfully() {
+		// Write code here that turns the phrase above into concrete actions
+
+		System.out.println("verify home page title");
+	}
+
 }
-
-@When("I try to login with username {string} and password {string}")
-public void i_try_to_login_with_username_and_password(String string, String string2) {
-    // Write code here that turns the phrase above into concrete actions
-
-    homePage.enterUsername("Admin");	
-    homePage.enterPassword("admin123");
-}
-
-@Then("I am logged in successfully")
-public void i_am_logged_in_successfully() {
-    // Write code here that turns the phrase above into concrete actions
-
-    homePage.clickLoginBtn();
-}
-
-}
-
-
