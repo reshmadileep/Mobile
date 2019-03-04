9eb58015609fc8f6d155faad996256b49ca7802b
naveen1295 <42132518+naveen1295@users.noreply.github.com>
2019-03-05 10:24:01 +1100

    flow to enter text and verify for ios app

--- src/test/java/pages/IOSAlertViewScreen.java
+++ src/test/java/pages/IOSAlertViewScreen.java
@@ -3,26 +3,45 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
 public class IOSAlertViewScreen {
-	
+
 	IOSDriver<?> driver;
+
 	public IOSAlertViewScreen(AppiumDriver<?> driver) {
 		this.driver = (IOSDriver<?>) driver;
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
-	
+
 	@FindBy(xpath = "//*[@value = 'Text Entry']")
 	public WebElement textentry;
-	
+
+	@FindBy(xpath = "//XCUIElementTypeOther")
+	public WebElement shorttitle;
+
+	@FindBy(xpath = "//XCUIElementTypeButton[@lable='OK']")
+	public WebElement btnok;
+
 	public IOSAlertViewScreen clicktextentry() {
 		textentry.click();
 		return this;
-		
+	}
+
+	public IOSAlertViewScreen entershorttitle(String shorttitlevalue) {
+		shorttitle.sendKeys(shorttitlevalue);
+		btnok.click();
+		return this;
+	}
+
+	public IOSAlertViewScreen verifyshorttitle(String expectedshorttitlevalue) {
+		clicktextentry();
+		Assert.assertEquals(shorttitle.getText(), expectedshorttitlevalue, "short title is not saved successfully");
+		return this;
 	}
 
 }
--- src/test/java/stepDefinitions/IOSApplication.java
+++ src/test/java/stepDefinitions/IOSApplication.java
@@ -13,14 +13,14 @@ import pages.IOSUICatalogScreen;
 import utils.ReusableFunctions;
 
 public class IOSApplication {
-	
+
 	private World world;
 	private AppiumDriver<?> driver;
 	private HashMap<String, String> map;
 	IOSUICatalogScreen uicataloghomescreen;
 	IOSAlertViewScreen alertscreen;
 	ReusableFunctions rfunctions;
-	
+
 	@SuppressWarnings("unchecked")
 	public IOSApplication(World world) {
 		this.world = world;
@@ -29,22 +29,23 @@ public class IOSApplication {
 		map = (HashMap<String, String>) world.context.get("config");
 		alertscreen = new IOSAlertViewScreen(driver);
 		uicataloghomescreen = new IOSUICatalogScreen(driver);
-		
+
 	}
-	
+
 	@Given("I am on the Alert Views Screen")
 	public void I_am_on_the_Alert_Views_Screen() {
 		uicataloghomescreen.clickalertviews();
 	}
 
-    @When("I try to save short title")
-    public void I_try_to_save_short_title(){
-    	alertscreen.clicktextentry();
-    	
-    }
+	@When("I try to save short title")
+	public void I_try_to_save_short_title() {
+		alertscreen.clicktextentry();
+		alertscreen.entershorttitle(map.get("ShortTitle"));
 
-    @Then("I am able to see my short title saved")
-    public void I_am_able_to_see_my_short_title_saved(){
-        System.out.println("in Then");
-    }
+	}
+
+	@Then("I am able to see my short title saved")
+	public void I_am_able_to_see_my_short_title_saved() {
+		alertscreen.verifyshorttitle(map.get("ShotTitle"));
+	}
 }
