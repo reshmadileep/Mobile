2a7770c0846b73d43be40b38b80cf9cc01b1eaef
naveen1295 <42132518+naveen1295@users.noreply.github.com>
2019-03-05 18:29:41 +1100

    login page xpath updates for ios

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -8,20 +8,18 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import stepDefinitions.World;
 
 public class HomePage {
 
-	@FindBy(id = "txtUsername")
+	@FindBy(xpath = "//*[@id='txtUsername'] OR //*[@id='credentials.j_username']")
 	private WebElement txtUserName;
 
-	@FindBy(id = "txtPassword")
+	@FindBy(xpath = "//*[@id ='txtPassword'] OR //*[@id='credentials.j_password']")
 	private WebElement txtPassword;
 
-	@FindBy(id = "btnLogin")
+	@FindBy(xpath = "//*[@id ='btnLogin'] OR //button[contains(@text(),'Log in']")
 	private WebElement btnLogin;
 
 	@FindBy(id = "dashboardTab")
